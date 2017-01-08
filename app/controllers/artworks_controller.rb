2e4db6a124ef3c4e5e121345d5caf4a4f1dce950
Anne Johnson <annekjohnson23@yahoo.com>
2017-01-07 20:39:47 -0500

    Add artwork image model, controller, and view updates

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -69,6 +69,6 @@ class ArtworksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def artwork_params
-      params.require(:artwork).permit(:name, :available, :price)
+      params.require(:artwork).permit(:name, :available, :price, :image)
     end
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,2 +1,8 @@
 class Artwork < ApplicationRecord
+  has_attached_file :image, styles: { thumb: "150x150>" }
+  validates_attachment(
+    :image,
+    presence: true,
+    content_type: { content_type: /\Aimage\/.*\z/ }
+  )
 end
